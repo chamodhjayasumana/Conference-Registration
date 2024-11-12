64f07b820d7a7403657896212095adb43597f80f
Chamodh Jayasumana <chamodhinduwarajayasumana@gmail.com>
2024-11-12 13:32:41 +0530

    Add project files.

---
+++ Conference Registration System/Home.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="Conference_Registration_System.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Welcome to the Conference Registration</h1>
+            <p>Event Date: 25th December 2024</p>
+            <p>Location: Conference Hall 1, City Center</p>
+
+            <%-- If user is logged in, display user info and logout link --%>
+            <asp:Label ID="lblGreeting" runat="server" Visible="false"></asp:Label><br />
+            <asp:Button ID="btnLogout" runat="server" Text="Logout" OnClick="btnLogout_Click" Visible="false" /><br />
+
+            <%-- If user is not logged in, show login link --%>
+            <asp:Button ID="btnLogin" runat="server" Text="Login" OnClick="btnLogin_Click" Visible="true" /><br />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Conference Registration System/Home.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Conference_Registration_System
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["UserName"] != null)
+            {
+                lblGreeting.Text = "Hello, " + Session["UserName"].ToString();
+                lblGreeting.Visible = true;
+                btnLogin.Visible = false;
+                btnLogout.Visible = true;
+            }
+            else
+            {
+                lblGreeting.Visible = false;
+                btnLogin.Visible = true;
+                btnLogout.Visible = false;
+            }
+        }
+
+        protected void btnLogout_Click(object sender, EventArgs e)
+        {
+            Session.Clear();
+            Response.Redirect("Home.aspx");
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Login.aspx");
+        }
+    }
+}
